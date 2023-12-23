0cfb6bbe0ff452a69cd40b886d20ce187e9307e5
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2023-12-23 14:32:40 +0900

    test

--- javaFinalProject/src/낙서장/낙서장.java
+++ javaFinalProject/src/낙서장/낙서장.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class 낙서장 {
 	public static void main(String[] args) {
 		Random r = new Random();
+		System.out.println("wwdwd");
 		System.out.println(r.nextInt(10));
 	}
 }
